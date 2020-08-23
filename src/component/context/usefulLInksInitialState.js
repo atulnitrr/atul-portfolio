e9c2d616a7a0fbca85d199dcf09363ad06413e9d
Atul Kumar <atul.kumar3@go-mmt.com>
2020-08-23 20:49:21 +0530

    Added data

--- src/component/context/usefulLInksInitialState.js
+++ src/component/context/usefulLInksInitialState.js
@@ -24,6 +24,12 @@ const usefulLinkInitialState = [
     url: "https://edgedb.com/blog/we-can-do-better-than-sql/",
     id: 5,
   },
+  {
+    title: "postgrs vs mysql",
+    url:
+      "https://developer.okta.com/blog/2019/07/19/mysql-vs-postgres#:~:text=Postgres%20Advantages%20over%20MySQL,more%20closely%20to%20SQL%20standards.",
+    id: 5,
+  },
 ];
 
 export default usefulLinkInitialState;
