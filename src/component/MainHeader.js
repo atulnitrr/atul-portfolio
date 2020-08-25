49409026895ce85d1ff92d381ac3bf778173a8d8
Atul Kumar <atul.kumar3@go-mmt.com>
2020-08-25 14:53:27 +0530

    main header

--- src/component/MainHeader.js
+++ src/component/MainHeader.js
@@ -4,7 +4,12 @@ import "../css/MainHeader.css";
 function MainHeader() {
   return (
     <header className="top-container-item tc-h">
+      <div></div>
       <h3>Atul's profile</h3>
+      <section>
+        <h6>atulkumar458@gmail.com</h6>
+        <h6>atulkumar4568@gmail.com</h6>
+      </section>
     </header>
   );
 }
