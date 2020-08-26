6ed71980a41f00e727a51050ad9538dc8b7e2e9c
Atul Kumar <atul.kumar3@go-mmt.com>
2020-08-26 18:59:00 +0530

    Added zinavi websites

--- src/component/context/usefulLInksInitialState.js
+++ src/component/context/usefulLInksInitialState.js
@@ -5,6 +5,11 @@ const usefulLinkInitialState = [
     id: 1,
   },
   {
+    title: "zainrizvi",
+    url: "https://www.zainrizvi.io/",
+    id: 7,
+  },
+  {
     title: "css layouts",
     url: "https://csslayout.io/patterns",
     id: 2,
@@ -28,7 +33,7 @@ const usefulLinkInitialState = [
     title: "postgrs vs mysql",
     url:
       "https://developer.okta.com/blog/2019/07/19/mysql-vs-postgres#:~:text=Postgres%20Advantages%20over%20MySQL,more%20closely%20to%20SQL%20standards.",
-    id: 5,
+    id: 6,
   },
 ];
 
