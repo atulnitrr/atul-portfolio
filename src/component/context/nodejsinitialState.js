97cd0f7efb3d095ece9a4fb8d6cd508c39b4c6ca
Atul Kumar <atul.kumar3@go-mmt.com>
2020-08-27 15:50:29 +0530

    psql setups

--- src/component/context/nodejsinitialState.js
+++ src/component/context/nodejsinitialState.js
@@ -11,6 +11,17 @@ const nodejsInitialState = [
       "https://itnext.io/how-to-use-environment-variables-in-node-js-cb2ef0e9574a",
     id: 2,
   },
+  {
+    title: "node pgsql",
+    url: "https://node-postgres.com/features/connecting",
+    id: 3,
+  },
+  {
+    title: "pgsql simple setup",
+    url:
+      "https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/",
+    id: 3,
+  },
 ];
 
 export default nodejsInitialState;
