1cbd1c70fe3fd13ba7ddfc8fa12f534e73fc8e11
Atul Kumar <atul.kumar3@go-mmt.com>
2020-09-12 10:46:00 +0530

    Added grokking the interview

--- src/component/context/designInitialState.js
+++ src/component/context/designInitialState.js
@@ -4,6 +4,23 @@ const designInitialState = [
     url: "http://highscalability.com/",
     id: 1,
   },
+  {
+    title: "freecodecamp system design",
+    url: "https://www.freecodecamp.org/news/systems-design-for-interviews/",
+    id: 2,
+  },
+  {
+    title: "how to succeed",
+    url:
+      "https://blog.pramp.com/how-to-succeed-in-a-system-design-interview-27b35de0df26",
+    id: 3,
+  },
+  {
+    title: "25 interview questions",
+    url:
+      "https://medium.com/javarevisited/25-software-design-interview-questions-to-crack-any-programming-and-technical-interviews-4b8237942db0",
+    id: 3,
+  },
 ];
 
 export default designInitialState;
